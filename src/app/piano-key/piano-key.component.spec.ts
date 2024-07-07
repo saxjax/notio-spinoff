8a9ee7165e49c8e76d20695bee8c13fea2cb48a2
saxjax <saxjax@saxjax.dk>
2024-07-07 22:41:17 +0200

    removing karma and jasmine, implementing jest. using hostComponent pattern for testing

--- src/app/piano-key/piano-key.component.spec.ts
+++ src/app/piano-key/piano-key.component.spec.ts
@@ -1,23 +1,57 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PianoKeyComponent } from './piano-key.component';
+import { TonePlayerService } from '../services/tone-player/tone-player.service';
+import { Component } from '@angular/core';
+import { EnglishToneNames } from '../constants/EnglishToneNames';
+import { By } from '@angular/platform-browser';
+
+// Create WrapperComponent
+@Component({
+  template: `
+    <piano-key [toneName]="pianoKey"></piano-key>
+    <p data-testid="txt-keyname">{{ pianoKey }}</p>
+  `,
+  standalone: true,
+  imports: [PianoKeyComponent],
+})
+class WrapperComponentPianoKey {
+  pianoKey = EnglishToneNames.A;
+}
+// End create WrapperComponent
 
 describe('PianoKeyComponent', () => {
-  let component: PianoKeyComponent;
-  let fixture: ComponentFixture<PianoKeyComponent>;
+  let component: WrapperComponentPianoKey;
+  let fixture: ComponentFixture<WrapperComponentPianoKey>;
+
+  const tonePlayerServiceMock = {
+    play: jest.fn(),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PianoKeyComponent]
-    })
-    .compileComponents();
+      imports:[WrapperComponentPianoKey],
+      providers: [{ provide: TonePlayerService, useValue: tonePlayerServiceMock }],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(PianoKeyComponent);
+    fixture = TestBed.createComponent(WrapperComponentPianoKey);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have key with value A', () => {
+    const keynameElement: HTMLParagraphElement = fixture.debugElement.query(
+      By.css('[data-testid=txt-keyname]')
+    ).nativeElement;
+    expect(keynameElement.textContent).toBe(EnglishToneNames.A.toString());
+  });
+
+  it('should call the tonePlayer service when the button is clicked', () => {
+    const button = fixture.debugElement.query(By.css('button')).nativeElement;
+    button.click();
+    expect(tonePlayerServiceMock.play).toHaveBeenCalledWith(EnglishToneNames.A);
   });
 });
--- src/app/piano-key/piano-key.component.ts
+++ src/app/piano-key/piano-key.component.ts
@@ -4,6 +4,7 @@ import { EnglishToneNames } from '../constants/EnglishToneNames';
 
 @Component({
   selector: 'piano-key',
+  template:`<button type="button" (click)="keyClicked()">{{toneName()}}</button>`,
   standalone: true,
   imports: [],
   templateUrl: './piano-key.component.html',
